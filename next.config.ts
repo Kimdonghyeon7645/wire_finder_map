943ef33852a5909c8b7b05b23d62baad35b3125b
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2026-03-19 15:10:57 +0900

    code : 프로젝트 생성

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "wire_finder_map";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
